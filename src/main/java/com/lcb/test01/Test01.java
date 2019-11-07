9b133484bfcc9deb2fa424d4da8d279bed011309
LCB <583491839@qq.com>
2019-11-07 16:25:35 +0800

    add bbb

--- src/main/java/com/lcb/test01/Test01.java
+++ src/main/java/com/lcb/test01/Test01.java
@@ -4,6 +4,6 @@ public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("helloaaa");
 		System.out.println("rightaaa");
-		System.out.println("helloaaa");
+		System.out.println("helloaaabbb");
 	}
 }
