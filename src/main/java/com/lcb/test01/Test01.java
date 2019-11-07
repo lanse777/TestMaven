38163cbff235d9525eb8f8f5a4134b9cf24c5157
LCB <583491839@qq.com>
2019-11-07 16:21:37 +0800

    add helloaaa

--- src/main/java/com/lcb/test01/Test01.java
+++ src/main/java/com/lcb/test01/Test01.java
@@ -2,8 +2,8 @@ package com.lcb.test01;
 
 public class Test01 {
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("helloaaa");
 		System.out.println("rightaaa");
-		System.out.println("hello");
+		System.out.println("helloaaa");
 	}
 }
