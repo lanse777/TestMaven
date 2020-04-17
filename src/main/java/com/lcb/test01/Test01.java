ca04a79d70d434defa03dea9bfdb6a422b795c7e
LCB <583491839@qq.com>
2020-04-17 15:21:08 +0800

    add time

--- src/main/java/com/lcb/test01/Test01.java
+++ src/main/java/com/lcb/test01/Test01.java
@@ -3,9 +3,6 @@ package com.lcb.test01;
 public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("helloaaa");
-		System.out.println("rightaaa");
-		System.out.println("helloaaabbb");
-		System.out.println("leftaaa");
-		System.out.println("hot_fix");
-	}
+        System.out.println("2020/4/17");
+    }
 }
