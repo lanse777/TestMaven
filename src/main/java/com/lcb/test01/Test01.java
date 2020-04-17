4f389ac15a38ce744949c175026c5ba71e87e44d
LCB <583491839@qq.com>
2020-04-17 16:21:33 +0800

    add code

--- src/main/java/com/lcb/test01/Test01.java
+++ src/main/java/com/lcb/test01/Test01.java
@@ -2,8 +2,24 @@ package com.lcb.test01;
 
 public class Test01 {
 	public static void main(String[] args) {
-		System.out.println("helloaaa");
-        System.out.println("2020/4/17");
-        System.out.println("aaaaaaa");
+	    String json = "{\"username\":\"jack\",\"username\":\"aaa\",\"username\":\"bbb\"}";
+        findName(json,"bbb");
+        System.out.println("aaaaaaaaaa");
+    }
+    public static void findName(String json, String targetName) {
+        // 去大括号
+        String substring = json.substring(1, json.length() - 1);
+        // 先按逗号拆分
+        String[] split = substring.split(",");
+
+        for (String s : split) {
+            String strings = s.split(":")[1];
+
+            if (strings.equals("\"" + targetName + "\"")){
+                System.out.println(targetName);
+                return;
+            }
+        }
+
     }
 }
