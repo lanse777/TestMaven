b1099a16d3a9758d4a90811bac8510db1894b907
LCB <583491839@qq.com>
2020-04-17 16:28:30 +0800

    add bbbbbb

--- src/main/java/com/lcb/test01/Test01.java
+++ src/main/java/com/lcb/test01/Test01.java
@@ -2,7 +2,24 @@ package com.lcb.test01;
 
 public class Test01 {
 	public static void main(String[] args) {
-		System.out.println("leftaaa");
-		System.out.println("hot_fix");
-	}
+	    String json = "{\"username\":\"jack\",\"username\":\"aaa\",\"username\":\"bbb\"}";
+        findName(json,"bbb");
+        System.out.println("aaaaaaaaaa");
+        System.out.println("bbbbbbb");
+    }
+    public static void findName(String json, String targetName) {
+        // 去大括号
+        String substring = json.substring(1, json.length() - 1);
+        // 先按逗号拆分
+        String[] split = substring.split(",");
+
+        for (String s : split) {
+            String strings = s.split(":")[1];
+
+            if (strings.equals("\"" + targetName + "\"")){
+                System.out.println(targetName);
+                return;
+            }
+        }
+    }
 }
